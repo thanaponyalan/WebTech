a50e4ea008802c4b56f092bc4cf35732d9a2544a
thanaponyalan <thanaponyalan@gmail.com>
2020-05-22 23:15:07 +0700

    comment myHangman

--- PWP/js/myHangman.js
+++ PWP/js/myHangman.js
@@ -1,7 +1,8 @@
+// assign words in each category
 var words=[
-    [`Mario`,`RedAlert`,`Contra`],
-    [`Harry Potter`,`Fantastic Beasts`,`Narnia`],
-    [`One Piece`,`Sword Art Online`,`Inazuma Eleven`]
+    [`Mario`,`RedAlert`,`Contra`], //Game Category
+    [`Harry Potter`,`Fantastic Beasts`,`Narnia`], //Movie Category
+    [`One Piece`,`Sword Art Online`,`Inazuma Eleven`] //Anime Category
 ];
 var word,wordHolder,holdArea,buttonArea;
 var guesses=[],
@@ -16,44 +17,45 @@ var reset=document.querySelector(`#reset`),
     showLives=document.querySelector(`#mylives`),
     categoryName=document.querySelector(`#categoryName`);
 
-function createButton(){
+function createButton(){ //Add 26 btns for 26 English alphabet, assign eventListener to each btn.
     var alphabets=[`a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`];
     buttonArea=document.querySelector(`#buttons`);
     var btn;
 
     for(var i=0;i<alphabets.length;i++){
-        btn=document.createElement(`button`);
-        btn.className=`letter`;
-        btn.innerHTML=alphabets[i];
-        btn.addEventListener(`click`,function(){
-            var guessed=this.innerHTML;
-            this.setAttribute(`class`,this.getAttribute(`class`)+` active`);
-            this.setAttribute(`disabled`,true)
-            for(var j=0;j<word.length;j++){
-                if(word[j].toLowerCase()===guessed){
-                    guesses[j].innerHTML=guessed;
-                    counter++;
+        btn=document.createElement(`button`); //create button elem.
+        btn.className=`letter`; //assign class for css decoration
+        btn.innerHTML=alphabets[i]; //assign each alphabet to each button
+        btn.addEventListener(`click`,function(){ //add evenlistener onclick
+            var guessed=this.innerHTML; //guessed alphabet
+            this.setAttribute(`class`,this.getAttribute(`class`)+` active`); //add class for css decoration
+            this.setAttribute(`disabled`,true) //disabled pressed button
+            for(var j=0;j<word.length;j++){ //compare guessed alphabet to each alphabet of the random word
+                if(word[j].toLowerCase()===guessed){ //guessed alphabet is only lower case so, make sure to transform the random word to lower case
+                    guesses[j].innerHTML=guessed; //guessed is array of HTML elem. show an alphabet if guessed true.
+                    counter++; //increase counter by 1; to check if game end.
                 }
             }
-            var j=(word.toLowerCase().indexOf(guessed));
-            if(j!==-1)showStatus();
+            var j=(word.toLowerCase().indexOf(guessed)); //check if guessed alphabet is part of the random word; -1 isn't part of the random word otherwise, it's part of the random word.
+            if(j!==-1)showStatus(); //show current status (remaining lives)
             else{
-                lives-=1;
+                lives-=1; //decrease live by 1 if guessed wrong.
                 showStatus();
-                animateHangman();
+                animateHangman(); //draw a part of hangman
             }
         });
-        buttonArea.appendChild(btn);
+        buttonArea.appendChild(btn); //append each button into buttonArea (element#buttons)
     }
 }
 
-function createHole(){
+function createHole(){ //create guessing area
     wordHolder=document.querySelector(`#hold`);
     holdArea = document.createElement('ul');
-    holdArea.setAttribute('id', 'my-word');
+    holdArea.setAttribute('id', 'my-word'); //assign id for css decoration
     for (var i = 0; i < word.length; i++) {
         var guess = document.createElement('li');
-        guess.setAttribute('class', 'guess');
+        guess.setAttribute('class', 'guess'); //assign class for css decoration
+        // letter is "-" or " "(blankspace) then show that letter and increase space by 1 to check if game end otherwise, show _ (underscore)
         if (word[i] === "-") {
             guess.innerHTML = "-";
             space += 1;
@@ -63,35 +65,41 @@ function createHole(){
         }else {
             guess.innerHTML = "_";
         }
-        guesses.push(guess);
-        wordHolder.appendChild(holdArea);
-        holdArea.appendChild(guess);
+        guesses.push(guess); //push each HTML element to guesses (array of HTML elem.)
+        wordHolder.appendChild(holdArea); //append holdArea (ul#my-word) into wordHolder (element#hold)
+        holdArea.appendChild(guess); //append each guess letter into holdArea
     }
 }
 
 function showStatus(){
+    //Normally show remaining live(s), 
+    //if live less than 1 then Game Over and add css class to avoid click on the other guess buttons and show Game Over in status elem.
+    //if game end (check if counter+space==length of word) show You Win! in status elem
     showLives.innerHTML=`You have `+lives+` live(s) remaining..`;
     if(lives<1){
         buttonArea.setAttribute(`class`,`avoid-clicks`)
         showLives.innerHTML=`Game Over`;
     }
-    for(var i=0;i<guesses.length;i++){
-        if(counter+space==guesses.length)showLives.innerHTML=`You Win!`;
+    if(counter+space==guesses.length){
+        buttonArea.setAttribute(`class`,`avoid-clicks`)
+        showLives.innerHTML=`You Win!`;
     }
+    
 }
 
 function startGame(){
+    //choose category by input a number
     while(chosenCategory==null||chosenCategory<1||chosenCategory>3||Array.isArray(chosenCategory))chosenCategory=parseInt(prompt(`Please select category\n[1] Game\n[2] Movie\n[3] Anime`,`1`));
     chosenCategory=words[chosenCategory-1];
-    word=chosenCategory[Math.floor(Math.random()*chosenCategory.length)];
-    lives=10;
-    space=counter=0;
-    guesses=[];
-    canvas();
+    word=chosenCategory[Math.floor(Math.random()*chosenCategory.length)]; //random a word
+    lives=10; //a player has 10 lives
+    space=counter=0; //space and counter (to check if game end) initial with 0
+    guesses=[]; //a array of HTML elem.
+    canvas(); //start to draw with canvas
     createButton();
     createHole();
     showStatus();
-    showCategory();
+    showCategory(); //show chosen category
 }
 
 function showCategory(){
@@ -102,7 +110,7 @@ function showCategory(){
 
 }
 
-function animateHangman(){
+function animateHangman(){ //draw each part of hangman depends on how many lives left.
     drawArray[lives]();
 }
 
@@ -128,7 +136,7 @@ function frame4(){
     draw(60,5,60,15);
 }
 
-function head(){
+function head(){ //draw head(circle) with arc
     myStickman=document.querySelector(`#stickman`);
     context=myStickman.getContext(`2d`);
     context.beginPath();
@@ -156,9 +164,9 @@ function leftLeg(){
     draw(60,70,20,100);
 }
 
-drawArray=[rightLeg, leftLeg, rightArm, leftArm, torso, head, frame4, frame3, frame2 ,frame1]
+drawArray=[rightLeg, leftLeg, rightArm, leftArm, torso, head, frame4, frame3, frame2 ,frame1]; //put each function into drawArray
 
-function canvas(){
+function canvas(){ //start canvas draw with color: #000 and width: 2
     myStickman=document.querySelector(`#stickman`);
     context=myStickman.getContext(`2d`);
     context.beginPath();
@@ -169,21 +177,21 @@ function canvas(){
 (function(){
     `use strict`;
     startGame();
-    hint.addEventListener(`click`,function(){
+    hint.addEventListener(`click`,function(){ //add event listener when hint button clicked
+        //array of hint (as same position as word position)
         hints=[
             [`Luigi`,`Command & Conquer`,`Produced by Konami in 1987`],
             [`Hogwarts`,`J.K.Rolling in 2016`,`A series of novels by C. S. Lewis.`],
             [`A Japanese shōnen manga series written and illustrated by Eiichiro Oda.`,`A Japanese light novel series written by Reki Kawahara and illustrated by abec.`,`The animated series was produced by OLM under the direction of Katsuhito Akiyama and consists of 127 episodes.`]
         ];
-        var categoryIndex=words.indexOf(chosenCategory);
-        var hintIndex=chosenCategory.indexOf(word);
-        showClue.innerHTML=`Clue : `+hints[categoryIndex][hintIndex];
+        var categoryIndex=words.indexOf(chosenCategory); //check index of chosenCategory in words
+        var hintIndex=chosenCategory.indexOf(word); //check index of word in chosenCategory
+        showClue.innerHTML=`Clue : `+hints[categoryIndex][hintIndex]; //show clue
     });
-    reset.addEventListener(`click`,function(){
-        holdArea.parentNode.removeChild(holdArea);
-        buttonArea.innerHTML=``;
-        showClue.innerHTML=``;
-        startGame();
+    reset.addEventListener(`click`,function(){ //add event listener when reset button clicked
+        holdArea.parentNode.removeChild(holdArea); //remove all guessed alphabets
+        buttonArea.innerHTML=``; //clear all button
+        showClue.innerHTML=``; //clear clue
+        startGame(); //start new game
     });
-    
 })();
